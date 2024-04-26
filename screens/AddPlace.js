a1e8892c77c4788a3a888651876bb48f1a049dc8
Anurag Shrestha <ashrestha@unm.edu>
2024-04-26 01:15:25 -0600

    This function displays the form of the place

--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -1,3 +1,14 @@
 /**
  * This file is responsible to add all the new places
  */
+
+
+import Form from "../components/form";
+
+function AddPlace(){
+    return(
+        <Form/>
+    )
+}
+
+ export default AddPlace;
