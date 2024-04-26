c03a6c8edb83306df817e0d24da7620fe8d685e3
Anurag Shrestha <ashrestha@unm.edu>
2024-04-26 01:16:43 -0600

    This function adds the list of places

--- screens/Places.js
+++ screens/Places.js
@@ -1,3 +1,13 @@
 /**
- * This class shows all the places that has been added.
+ * This file shows all the places that has been added.
  */
+
+import PlacesLists from "../components/PlacesLists";
+
+function Places({places}){
+    return(
+        <PlacesLists/>
+    )
+}
+
+export default Places;
