5e7f4ddd39197b1ac466f3671562b3c07ca96ba2
Anurag Shrestha <ashrestha@unm.edu>
2024-04-26 01:14:30 -0600

    checked if any places has been added or not

--- components/PlacesLists.js
+++ components/PlacesLists.js
@@ -2,10 +2,20 @@
  * This file is responsible to shows all the places as a list.
  */
 
-import { FlatList } from "react-native";
+import { FlatList, View, StyleSheet,Text } from "react-native";
 import Item from "./Item";
 
 function PlacesLists({ places }) {
+
+    if(!places || places.length == 0){
+        return(
+            <View style = {styles.container}>
+                <Text style = {styles.empty}>
+                    No places has been added till now. Please add some places here.
+                </Text>
+            </View>
+        )
+    }
   return (
     <FlatList
       data={places}
@@ -14,5 +24,19 @@ function PlacesLists({ places }) {
     />
   );
 }
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    empty: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'maroon',
+    }
+})
 
 export default PlacesLists;
