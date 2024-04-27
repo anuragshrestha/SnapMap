8c3049c873207042424a11acd98abe6a581ca7bc
Anurag Shrestha <ashrestha@unm.edu>
2024-04-27 16:55:06 -0600

    added textInput section

--- components/form.js
+++ components/form.js
@@ -1,10 +1,24 @@
-import { Text, StyleSheet } from "react-native";
+import { useScrollToTop } from "@react-navigation/native";
+import { useState } from "react";
+import { Text, StyleSheet, ScrollView, View, TextInput } from "react-native";
 
 function Form(){
+
+ const[enteredTitle, setEnteredTitle] = useState(' ');
+
+  function changeText(enteredText){
+    setEnteredTitle(enteredText);
+  }
+
     return (
-        <Text style = {styles.conatiner}>
-            Add a place here
-        </Text>
+        <ScrollView style = {styles.form}>
+            <View>
+                <Text style = {styles.conatiner}>
+                    Title
+                </Text>
+                <TextInput style = {styles.input}  onChangeText={changeText} value= {enteredTitle} />
+            </View>
+        </ScrollView>
     )
 }
 
@@ -17,6 +31,22 @@ const styles = StyleSheet.create({
         fontSize: 20,
         textAlign: 'center',
         fontWeight: 'bold',
-        margin: 100
+        marginBottom: 4
+    },
+    form:{
+        flex: 1,
+        padding: 24,
+    },
+    input:{
+     marginVertical: 8,
+     paddingHorizontal: 4,
+     paddingVertical: 8,
+     fontSize: 16,
+     borderColor: 'darkblue',
+     borderWidth: 3,
+     color: 'black',
+     fontWeight : 'bold',
+     backgroundColor: 'skyblue',
+     borderRadius: 8
     }
 })
