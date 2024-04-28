a87d7b5fa4e747fc6964a877cc27fae28c52812e
Anurag Shrestha <ashrestha@unm.edu>
2024-04-27 19:20:31 -0600

    Created a custom button

---
+++ UI/Button.js
@@ -0,0 +1,47 @@
+/**
+ * This file creates a custom button
+ */
+
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+function Buttons({ icon, title, onPress }) {
+  return (
+    <Pressable
+      style={({ pressed }) => [styles.container, pressed && styles.pressed]}
+      onPress={onPress}
+    >
+        <Ionicons style = {styles.icon} name= {icon} size={18} color= 'white' />
+      <Text style={styles.text}> {title} </Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    textAlign: "center",
+    justifyContent: "center",
+    margin: 4,
+    paddingVertical: 6,
+    borderWidth: 2,
+    flexDirection: "row",
+   borderColor: 'skyblue',
+   borderRadius: 6,
+   maxWidth: 160,
+   marginLeft: 100
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+  text: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+
+  },
+  icon:{
+    padding: 2,
+  }
+});
+
+export default Buttons;
