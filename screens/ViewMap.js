6dc7815e7fbecc9c8b75cf2049d72be6bfd6ed92
Anurag Shrestha <ashrestha@unm.edu>
2024-05-15 10:01:08 -0600

    added a marker to select a location on the map

--- screens/ViewMap.js
+++ screens/ViewMap.js
@@ -5,7 +5,10 @@
 
 import MapView, { Marker } from "react-native-maps";
 import { Text, StyleSheet, View } from "react-native";
+import { useState } from "react";
 function ViewMap() {
+  const [selectedLocation, setSelectedLocation] = useState();
+
   const region = {
     latitude: 37.78,
     longitude: -122.43,
@@ -13,14 +16,36 @@ function ViewMap() {
     longitudeDelta: 0.0421,
   };
 
-  return <MapView style = {styles.view} initialRegion={region}></MapView>;
+  function clickLocationHandler(event) {
+    const latitude = event.nativeEvent.coordinate.latitude;
+    const longitude = event.nativeEvent.coordinate.longitude;
+
+    setSelectedLocation({ lat: latitude, long: longitude });
+  }
+
+  return (
+    <MapView
+      style={styles.view}
+      initialRegion={region}
+      onPress={clickLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="Select a Location"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.long,
+          }}
+        />
+      )}
+    </MapView>
+  );
 }
 
 const styles = StyleSheet.create({
-    view:{
-        flex: 1,
-    }
-})
-
+  view: {
+    flex: 1,
+  },
+});
 
 export default ViewMap;
