5e75e0ffc807a9077c2f287a15b29b23c3c243fe
Anurag Shrestha <ashrestha@unm.edu>
2024-05-16 12:09:33 -0600

    added the places in the main screen when user saves the place

--- screens/Places.js
+++ screens/Places.js
@@ -2,11 +2,25 @@
  * This file shows all the places that has been added.
  */
 
+import { useEffect, useState } from "react";
 import PlacesLists from "../components/PlacesLists";
+import { useIsFocused } from "@react-navigation/native";
 
-function Places({place}){
+function Places({route}){
+
+   const[updatedPlaces, setUpdatedPlaces] = useState([]);
+   const isFocused = useIsFocused();
+
+   useEffect(() => {
+      if(isFocused && route.params){
+        setUpdatedPlaces((currentPlaces) => [...currentPlaces, route.params.place])
+      }
+   }, [isFocused, route]);
+
+
+
     return(
-        <PlacesLists/>
+        <PlacesLists places={updatedPlaces} />
     )
 }
 
