57f55a28e59aa161a59d3e871d22e91e9382dc28
Anurag Shrestha <ashrestha@unm.edu>
2024-05-16 13:27:37 -0600

    added styling to the image and text

--- components/Item.js
+++ components/Item.js
@@ -2,19 +2,68 @@
  * This function is responsible to render each places and how they are render
  */
 
-
-import { Pressable, View,Image } from "react-native";
+import { Pressable, View, Image } from "react-native";
+import { Text, StyleSheet } from "react-native";
 
 function Item({ place, pressed }) {
   return (
-    <Pressable onPress={pressed} >
-      <Image source={{uri: place.imageUrl}} />
-      <View>
-        <Text>{place.title}</Text>
-        <Text>{place.address}</Text>
+    <Pressable
+      style={({ pressed }) => [styles.conatiner, pressed && styles.press]}
+      onPress={pressed}
+    >
+      <Image style={styles.image} source={{ uri: place.imageurl }} />
+      <View style={styles.view}>
+        <Text style={styles.title}>{place.title}</Text>
+        <Text style={styles.text}>Latitude: {place.location?.lat}</Text>
+        <Text style={styles.text}>Longitude: {place.location?.long}</Text>
       </View>
     </Pressable>
   );
 }
+
+const styles = StyleSheet.create({
+  conatiner: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    shadowOffset: {width: 1, height: 1},
+    shadowRadius: 2,
+    shadowOpacity: 0.15,
+    shadowColor: 'black',
+    borderRadius: 6,
+    elevation: 2,
+    marginVertical: 12,
+    backgroundColor: 'darkblue',
+    borderColor: 'black',
+    borderWidth: 2,
+    height: 130,
+  },
+  press: {
+    opacity: 0.9,
+  },
+  image: {
+    height: '100%',
+    width: 130,
+    borderBottomLeftRadius: 6,
+    borderTopLeftRadius: 6,
+  },
+  view: {
+    flex: 2,
+    flexDirection: 'column',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "red",
+    marginLeft: 10,
+
+  },
+  text: {
+    fontSize: 20,
+    color: "white",
+    marginLeft: 15,
+    marginTop: 15,
+    paddingTop: 5
+  },
+});
 
 export default Item;
