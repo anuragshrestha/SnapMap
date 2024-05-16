ee8ec70462def9667b5ecc3ae7a5bec5fb1c57eb
Anurag Shrestha <ashrestha@unm.edu>
2024-05-16 11:29:42 -0600

    updated the parameter type

--- structure/place.js
+++ structure/place.js
@@ -4,12 +4,12 @@
  */
 
 
-class Place{
-    constructor(title, imageurl, address, location){
+export class Place{
+    constructor(title, imageurl,location){
         this.title = title;
         this.imageurl = imageurl;
-        this.address = address;
-        this.location = location; // it has latitude and longitude
+        this.address = location.address;
+        this.location = {lat: location.lat, long: location.long}; // it has latitude and longitude
         this.id = new Date().toString() + Math.random().toString();
     }
 }
